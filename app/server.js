48785af6e366407c7fbaa7931021fea5c0152623
Narayan <bhatj1964@gmail.com>
2021-05-25 13:29:36 +0530

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body());
+})
+
+app.listen(4567, () => console.log('Listening on port 4567'))
