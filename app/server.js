0e32f40f01766e59f240e77b07f0e16222ac094e
Narayan <bhatj1964@gmail.com>
2021-05-25 14:25:28 +0530

    Added body parser

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-    console.log(req.body());
+    console.log(req.body);
 })
 
 app.listen(4567, () => console.log('Listening on port 4567'))
